e24f13e5106b4b5125b6f7ef5f675a1a07d95d6d
parseb <petra306@protonmail.com>
2022-07-31 22:01:11 +0300

    adds test gassy, createsJar

--- src/GasJar.sol
+++ src/GasJar.sol
@@ -3,38 +3,68 @@ pragma solidity ^0.8.13;
 
 
 /// @notice ether native inprotocol usage tax
-/// @dev Experimental. Do not use.
+/// @dev Experimental PlayyThing. Do not use.
 /// @custom:security contact: petra306@protonmail.com
 /// @author parseb (https://github.com/parseb/gasjar/src/GasJar.sol)
 abstract contract GasJar {
 
+uint immutable MAXU = type(uint).max -2;
 /// 
 mapping(bytes4 => uint) sigInterations;
+mapping(address => uint[]) shelf;
+address public jarOwner;
 
-address jarOwner;
+/// temp, maybe
+uint[3] gassy; 
 
 /// Events
-   event jarOnwerChanged(address _prev, address _next);
-
+   event jarOnwerChanged(address prev_, address next_);
+   event createdJar(address contractaddress_, bytes4 sign_);
 
    constructor() { jarOwner = msg.sender;}
 
    modifier isJar() {
+    gassy[0] = tx.gasprice;
     if (jarOwner == address(0)) {
     _;
     } else {
+      gassy[1] = gasleft();
       // odo the do and render true
+      _doTheDo(jarOwner, sigInterations[msg.sig]);
       _;
+      gassy[2] = gasleft();
     }
    }
 
 
- function setJarOwner(address _o) public {
+ function setJarOwner(address o_) external {
    require (jarOwner != address(0));
-   jarOwner = _o;
+   jarOwner = o_;
 
-   emit jarOnwerChanged(msg.sender, _o);
+   emit jarOnwerChanged(msg.sender, o_);
  }
 
+ function createJar(bytes4 fxSig_, uint iterations_) external {
+  require( msg.sender == jarOwner, "Not Jar Owner");
+
+  sigInterations[fxSig_] = iterations_;
+
+  emit createdJar(address(this), fxSig_);
+ }
+
+
+function _doTheDo(address to_, uint iterNum_) private {
+  uint i = 1;
+  for (i; i < iterNum_;) {
+    shelf[to_].push( MAXU / i );
+    unchecked { ++i; }
+  }
+}
+
+function getGassy() external view returns (uint[3] memory zzz){
+  zzz[0] = gassy[0];
+  zzz[1] = gassy[1];
+  zzz[2] = gassy[2];
+}
 
 }
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -2,11 +2,42 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "../src/GasJar.sol";
+import "../src/Jarrable.sol";
+
+
 
 contract ContractTest is Test {
-    function setUp() public {}
 
-    function testExample() public {
-        assertTrue(true);
+    address OW;
+    UsesJar Jar;
+
+    function setUp() public {
+
+        OW = address(9);
+        vm.prank(OW);
+        Jar = new UsesJar();
+
     }
+
+    function testDeployerIsHJarOwner() public {
+        assertTrue(Jar.jarOwner() == OW, "Jar owner not deployer");
+    }
+
+    function testLastGas() public {
+        uint[3] memory gassy;
+        gassy = Jar.getGassy();
+        assertTrue(gassy[0] == gassy[1] && gassy[1] == gassy[2] && gassy[2] == 0, "Gassy not zero");
+        bytes4 plusOneSig = 0xf5a6259f;
+        vm.prank(OW);
+        Jar.createJar(plusOneSig, 234);
+        
+        vm.prank(OW);
+        assertTrue(Jar.plusOne(33) == 34, "does not plus one");
+
+        gassy = Jar.getGassy();
+        assertFalse(gassy[0] == gassy[1] && gassy[1] == gassy[2] && gassy[2] == 0, "Gassy not changed");
+
+    }
+    
 }
